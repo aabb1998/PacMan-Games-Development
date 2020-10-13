645bd4614586a81560161628018ab966a97567fb
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2020-10-13 22:51:33 +1100

    MainMenu Audio Source Addition

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
+
+    public AudioClip menu;
+    public AudioSource audio;
+
     // Start is called before the first frame update
     public void PlayLevel1() {
         SceneManager.LoadScene("SampleScene");
     }
+
+    // void Start() {
+    //     audio = GetComponent<AudioSource>();
+    //     audio.clip = menu;
+    //     audio.loop = true;
+    //     audio.Play();
+    // }
 }
