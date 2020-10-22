1999a67d22d8c249fb8f7346b8cd3c8890652008
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2020-10-23 01:40:57 +1100

    Added highscore functionality.

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    
+    public Text score;
+    //public Text time;
+    public Text highScore;
+    public Text bestTime;
+
+    void Start() {
+        highScore.text = "Score: " + PlayerPrefs.GetInt("Highscore", 30).ToString();
+    }
+
+    public void updateScore () {
+        int highscore = 10;
+        //Text time = System.DateTime;
+        score.text = "Score: " + highscore.ToString();
+
+        PlayerPrefs.SetInt("Highscore", highscore);
+
+    }
+
+}
